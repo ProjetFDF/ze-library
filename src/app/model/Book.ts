c65d7893248368d3ef78783f284c34d77104e452
PC-JC\Gwenhael <gwenhael.baccon@gmail.com>
2018-03-21 20:07:31 +0100

    [GWEN] - Prototypage de la fonctionnalité d'emprunt d'un user

--- src/app/model/Book.ts
+++ src/app/model/Book.ts
@@ -16,6 +16,7 @@ export interface Book {
     category: number;
 	editor: number;
 	authors: Array<number>;
+	ilEstEmprunter: boolean;
 
 }
 
--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanierComponent implements OnInit {
 
+  arrayBookPanier: Array<number>;   // null
+
   constructor() { }
 
   ngOnInit() {
+    this.arrayBookPanier = new Array<number>(); // []
+
+
+    // Récupérer la liste des livres 
+
+    // Pour chaque élement de la liste de livres tu met le boolean "ilEstEmprunter" à false
+  }
+
+  taFonctionEmprunter(idBook: number) {
+    this.arrayBookPanier.push(idBook); // [1]
+    // Je parcours la liste des livres "books" et trouver celui qui correspond à mon identifiant "idBook" transmis à ma fonction 
+    // Il faut passer l'object Book avec l'attribut "ilEstEmprunter" à true
+  }
+
+  taFonctionNePlusEmprunter(idBook: number) {
+    // Je parcours la liste des books
+    for(let i = 0; i < this.arrayBookPanier.length; i++) {
+      // Si mon id transmis est l'ID qui est parcouru dans mon array de Book
+      if (idBook === this.arrayBookPanier[i]) {
+        this.arrayBookPanier.splice(i, 1);    // Avant j'avais [1,2] après j'ai [2]            
+      }
+    }
+
+    // Je parcours la liste des livres "books" et trouver celui qui correspond à mon identifiant "idBook" transmis à ma fonction 
+    // Il faut passer l'object Book avec l'attribut "ilEstEmprunter" à false
+    
+  }
+
+  validerLePanier() {
+    // Appel du back avec transmission de la liste des bouquins empruntés "arrayBookPanier"
   }
 
 }
