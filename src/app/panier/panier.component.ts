db0dc3dd53e227a1b0b8185dbba762934c324a6e
MickaelGUENOT <mickael.guenot@infotel.com>
2018-03-22 18:01:34 +0100

    maj pannier

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { BackEndService } from '../service/back-end.service';
+import { MessagesService } from '../service/messages.service';
+import { DatashareService } from '../service/datashare.service';
+import { Book } from '../model/Book';
+import { Member } from '../model/Member'
 
 @Component({
   selector: 'app-panier',
@@ -7,31 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanierComponent implements OnInit {
 
-  arrayBookPanier: Array<number>;   // null
+  bookBasketIds: Array<number> = [];   // null
+  books: Array<Book> = [];
 
-  constructor() { }
+  member:Member;
+
+    constructor(
+    private backService : BackEndService,
+    private messageService: MessagesService,
+    private dss: DatashareService
+    ) { }
 
   ngOnInit() {
-    this.arrayBookPanier = new Array<number>(); // []
+   //this.arrayBookPanier = new Array<number>(); // []
 
+   
+   
 
     // Récupérer la liste des livres 
 
-    // Pour chaque élement de la liste de livres tu met le boolean "ilEstEmprunter" à false
   }
 
-  taFonctionEmprunter(idBook: number) {
-    this.arrayBookPanier.push(idBook); // [1]
+
+  emprunter(idBook: number) {
+    this.bookBasketIds.push(idBook); // [1]
     // Je parcours la liste des livres "books" et trouver celui qui correspond à mon identifiant "idBook" transmis à ma fonction 
     // Il faut passer l'object Book avec l'attribut "ilEstEmprunter" à true
   }
 
-  taFonctionNePlusEmprunter(idBook: number) {
+  enleverDuPanier(idBook: number) {
     // Je parcours la liste des books
-    for(let i = 0; i < this.arrayBookPanier.length; i++) {
+    for(let i = 0; i < this.bookBasketIds.length; i++) {
       // Si mon id transmis est l'ID qui est parcouru dans mon array de Book
-      if (idBook === this.arrayBookPanier[i]) {
-        this.arrayBookPanier.splice(i, 1);    // Avant j'avais [1,2] après j'ai [2]            
+      if (idBook === this.bookBasketIds[i]) {
+        this.bookBasketIds.splice(i, 1);    // Avant j'avais [1,2] après j'ai [2]            
       }
     }
 
@@ -43,5 +57,27 @@ export class PanierComponent implements OnInit {
   validerLePanier() {
     // Appel du back avec transmission de la liste des bouquins empruntés "arrayBookPanier"
   }
+
+   getListBookByMembersBasket()
+ {
+    this.backService.GetListBookBasketByMember(this.member).subscribe(
+     data => {
+       this.backService.handleData(data);
+       if (data.payload) {
+         console.log(data.payload);
+         //cache the logged member in datashare service
+         this.books = data.payload;
+         return this.bookBasketIds;
+       }
+     },
+     error => {
+       console.error(error.message);
+       //messageService.displayFailureMessage(error.message);
+       return null;
+     }
+
+   );
+
+ }
 
 }
